a96b988a2d15c84fb5afb091377711d17ace199f
Joe Sweeny <joesweeny84@hotmail.com>
2016-08-30 16:34:42 +0100

    User can complete fields

--- pinder/app/controllers/profiles_controller.rb
+++ pinder/app/controllers/profiles_controller.rb
@@ -5,6 +5,19 @@ class ProfilesController < ApplicationController
   end
 
   def new
+    @profile = Profile.new
   end
+
+  def create
+    Profile.create(profile_params)
+    redirect_to '/profiles'
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:owner_name, :owner_age, :owner_likes, :owner_dislikes, :pet_name, :pet_age, :pet_species, :pet_likes, :pet_dislikes)
+  end
+
 
 end
--- pinder/spec/features/profiles_feature_spec.rb
+++ pinder/spec/features/profiles_feature_spec.rb
@@ -13,5 +13,20 @@ feature "Profile creation" do
       expect(current_path).to eq('/profiles/new')
     end
 
+    scenario "A user can fill out the profile form" do
+      visit '/profiles'
+      click_link "Create Profile"
+      fill_in "Owner name", with: "Adam"
+      fill_in "Owner age", with: "24"
+      fill_in "Owner likes", with: "Country Music, Big Women and Folk Dancing"
+      fill_in "Owner dislikes", with: "Joe Sweeny, golf and cheese"
+      fill_in "Pet name", with: "Finn"
+      fill_in "Pet age", with: 2
+      fill_in "Pet species", with: "Dog - Golden Retriever"
+      fill_in "Pet likes", with: "Carrots"
+      fill_in "Pet dislikes", with: "Bald men"
+      click_button "Submit Profile"
+      expect(page).to have_content "Adam"
+    end
   end
 end
