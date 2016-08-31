14215841e9bca5dc8ab704f626683d766597b82a
Joe Sweeny <joesweeny84@hotmail.com>
2016-08-31 14:06:09 +0100

    Users need to be signed in to view profiles

--- pinder/spec/features/play_features_spec.rb
+++ pinder/spec/features/play_features_spec.rb
@@ -9,4 +9,14 @@ feature "Playing pinder" do
       expect(page).to have_css("//img[@src*='banana.jpg']")
     end
   end
+
+  context 'User needs to be signed in' do
+    scenario 'user must be signed in to view profiles' do
+      sign_up
+      create_profile
+      click_link 'Sign out'
+      expect(page).not_to have_content 'Adam'
+    end
+  end
+
 end
