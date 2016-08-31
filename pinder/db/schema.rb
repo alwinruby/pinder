af68181a993fe82f5a223086e22c4f1fd005b85c
Harry Jaggs <harryjaggs92@gmail.com>
2016-08-31 11:54:52 +0100

    user reference in profiles

--- pinder/db/schema.rb
+++ pinder/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831093955) do
+ActiveRecord::Schema.define(version: 20160831101201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,10 @@ ActiveRecord::Schema.define(version: 20160831093955) do
     t.datetime "pet_image_updated_at"
     t.string   "gender"
     t.string   "sexual_preference"
+    t.integer  "user_id"
   end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -60,4 +63,5 @@ ActiveRecord::Schema.define(version: 20160831093955) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "profiles", "users"
 end
