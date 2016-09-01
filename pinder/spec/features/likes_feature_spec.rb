5a0987cae6f5d66107d99991f004f349c278d3a5
Harry Jaggs <harryjaggs92@gmail.com>
2016-09-01 15:47:24 +0100

    viewing profiles dislike button spec - not passing

--- pinder/spec/features/likes_feature_spec.rb
+++ pinder/spec/features/likes_feature_spec.rb
@@ -1,20 +1,6 @@
 require "rails_helper"
 
 feature "Like button" do
-  # context "A user wants to like another user" do
-  #   scenario "A user likes another user's pet" do
-  #     sign_up
-  #     create_profile
-  #     visit '/'
-  #     click_link "Play"
-  #     click_button "Like"
-  #     click_button "Like!"
-  #     expect(page).to have_content("Likes: 1")
-  #   end
-  # end
-
-
-
   context "A user wants to like another user" do
 
     before do
@@ -35,6 +21,21 @@ feature "Like button" do
       click_button "Like"
       click_button 'Like'
       expect(page).to have_content("Buddy")
+    end
+  end
+end
+
+feature 'Dislike button' do
+  context "A user wants to dislike another user" do
+    before do
+      create_first_profile
+      create_second_profile
+    end
+    scenario "A user dislikes another user's pet" do
+      sign_up
+      create_profile
+      click_button "Dislike"
+      expect(page).to have_content("Dave")
     end
   end
 end
